93f42a8bc64e8f170ca8b68d983e7d4b64087f1c
akpaswan67 <arunkumarpaswn38@gmail.com>
2025-04-06 02:05:33 +0530

    final commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
 export const connect = async () =>{
-    await mongoose.connect('mongodb://localhost/twitter_dev');
+    await mongoose.connect('mongodb://localhost:27017/twitterdev');
 }
